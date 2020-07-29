da1d98dd2bd8f2e894a06c220415eb4eeaa782c4
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-07-29 13:31:26 -0500

    button restaurant

--- app/src/main/java/com/bid/closer/MainActivity.java
+++ app/src/main/java/com/bid/closer/MainActivity.java
@@ -3,6 +3,10 @@ package com.bid.closer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton buttonRestaurant = findViewById(R.id.buttonRestaurant);
+
+        buttonRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Pulsaste restaurantes", Toast.LENGTH_SHORT).show();
+                ImageView mapImg = findViewById(R.id.map);
+                mapImg.setImageResource(R.drawable.mapa_restaurantes);
+            }
+        });
     }
 }
